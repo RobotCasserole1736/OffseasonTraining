523639ebce5d8057ce13880c8f2f4d347ca39c7a
AstroCool <astrocoolguy1@gmail.com>
2020-10-15 20:05:54 -0500

    throughwith booleans in LessonTwo

--- Chapter_1/src/main/java/frc/robot/LessonTwo.java
+++ Chapter_1/src/main/java/frc/robot/LessonTwo.java
@@ -10,6 +10,13 @@ class LessonTwo {
     boolean sampleBool = true;
     int sampleInt = 42;
     double sampleDouble = -1736.930;
+    
+    double val1;
+    double val2;
+    double val3;
+    double val4;
+    double val5;
+    double val6;
 
     @Signal
     double result1;
@@ -33,8 +40,22 @@ class LessonTwo {
 
         ////////////////////////////////////////////////
         // Write your new code after this line...
+        val1= -34;
+        val2= 3.5;
+        val3= 76;
+        result1= val1+val2+val3;
 
+        val4=20;
+        val5=11;
+        val6=0.25;
+        result2 = (val4-val5)/val6;
 
+        
+        if(result1 == 45.5 & result2 == 36 ) {
+            result3 = true;
+        } else {
+            result3 = false;
+        }
         // ...but before this line.
         ////////////////////////////////////////////////
 
