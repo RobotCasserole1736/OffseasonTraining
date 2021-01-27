68f5331abe1e77bb1576632fadc032431207d5c0
CA-1234 <alcarazc2020@gmail.com>
2021-01-26 19:03:33 -0600

    LessonTwo

--- Chapter_1/src/main/java/frc/robot/LessonTwo.java
+++ Chapter_1/src/main/java/frc/robot/LessonTwo.java
@@ -10,36 +10,57 @@ class LessonTwo {
     boolean sampleBool = true;
     int sampleInt = 42;
     double sampleDouble = -1736.930;
+    int val1= 6;
+    int val2= -3;
+    double val3 = 5.5;
+    boolean val4= true;
+    boolean val5= true;
+    boolean val6= false;
+    int val7= 50;
+    int val8= 25;
 
-    @Signal
-    double result1;
+}
 
-    @Signal
-    double result2;
+@Signal
+double result1;
+double result1= (val1+val2+val3);
 
-    @Signal
-    boolean result3;
+@Signal
+double result2;
+double result2= ((val1/val2)*val3)*-4;
 
-    @Signal
-    boolean raiseArmNow;
+@Signal
+boolean result3;
+boolean result3= val4||val6;
 
-    @Signal
-    int result4;
+@Signal
+boolean raiseArmNow;
+boolean raiseArmNow=false;
 
+@Signal
+int result4;
+int result4= val7/val8;
+
+// ...but before this line.
+////////////////////////////////////////////////
+
+void lessonTwoInit(){
+
+    ////////////////////////////////////////////////
+    // Write your new code after this line...
+    
+    
+    if(val6);
+        {
+        raiseArmNow =false;
+        }if(val5 |val4);
+        raiseArmNow = true;
+        }
     // ...but before this line.
     ////////////////////////////////////////////////
 
-    void lessonTwoInit(){
-
-        ////////////////////////////////////////////////
-        // Write your new code after this line...
 
 
-        // ...but before this line.
-        ////////////////////////////////////////////////
-
-
-
-    }
+}
 
 }
