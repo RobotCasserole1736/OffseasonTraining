950b117f8f0b4a605e11b2dcff752b6e7096c62e
ASingleToast <jresrez@gmail.com>
2021-11-02 18:39:54 -0500

    WIP driver interface

--- Chapter_2/src/main/java/frc/robot/DriverInterface.java
+++ Chapter_2/src/main/java/frc/robot/DriverInterface.java
@@ -1,5 +1,19 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+
 public class DriverInterface {
     
+    XboxController driverController;
+
+    @Signal
+    double curFwdRevCmd;
+    @Signal
+    double curRotCmd
+
+    public DriverInterface(){
+
+        
+
+    }
 }
