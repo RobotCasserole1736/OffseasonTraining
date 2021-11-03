8a17c0f5dd49e3cf92e3fb45542a05b36453d976
Chris Gerth <chrisgerth010592@gmail.com>
2021-11-02 19:28:50 -0500

    wip adding drivetrain

---
+++ Chapter_2/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,46 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+import frc.lib.DataServer.Annotations.Signal;
+
+public class Drivetrain {
+
+    @Signal
+    double curFwdRevCmd = 0;
+    @Signal
+    double curRotateCmd = 0;
+
+    Spark motorCtrl_Left1;
+    Spark motorCtrl_Left2;
+    Spark motorCtrl_Left3;
+
+    Spark motorCtrl_Right1;
+    Spark motorCtrl_Right2;
+    Spark motorCtrl_Right3;
+
+    // Constructor
+    public Drivetrain(){
+
+        motorCtrl_Left1 = new Spark(7);
+        motorCtrl_Left2 = new Spark(8);
+        motorCtrl_Left3 = new Spark(9);
+
+        motorCtrl_Right1 = new Spark(4);
+        motorCtrl_Right2 = new Spark(5);
+        motorCtrl_Right3 = new Spark(6);
+
+    }
+
+    public void update(){
+
+    }
+
+    public void setFwdRevCmd(double cmd_in){
+        curFwdRevCmd = cmd_in;
+    }
+
+    public void setRotateCmd(double cmd_in){
+        curRotateCmd = cmd_in;
+    }
+    
+}
